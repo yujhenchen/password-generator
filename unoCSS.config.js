299fb08174b1def7a1d5398f998ee954ed95dcde
yujhenchen <yujhen.chen93@gmail.com>
2023-11-23 16:59:00 +0800

    add dark mode style, add color template

---
+++ unoCSS.config.js
@@ -0,0 +1,34 @@
+window.__unocss = {
+  // rules: [
+  //   // custom rules...
+  // ],
+  // presets: [
+  //   // custom presets...
+  // ],
+  // // ...
+  theme: {
+    // ...
+    colors: {
+      brand: {
+        night: "",
+        white: "",
+        mediumGray: "",
+        dark: "#1C1C1C",
+        green: {
+          veryHeavy: "#1F2937",
+          heavy: "#273549",
+          semiHeavy: "#2F3E53",
+          regular: "#10B981",
+          light: "#4ADF86",
+          semiLight: "#55F991",
+          veryLight: "#5DEF92",
+        },
+        coco: {
+          light: "#ECFDF5",
+          medium: "#D5D4D8",
+          heavy: "#6B7280",
+        },
+      },
+    },
+  },
+};
